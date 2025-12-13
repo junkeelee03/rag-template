192eb94b14c1aa0ccbb73b5d9f99da5e8e83cd6e
Jocelyn <a01285780@itesm.mx>
2025-12-13 14:54:32 -0600

    finished rag command method

--- backend.py
+++ backend.py
@@ -18,3 +18,24 @@ def get_vector_store():
     embeddings = GoogleGenerativeAIEmbeddings(model="model/embeddings-001")
     vector_store = MongoDBAtlasVectorSearch(collection=collection, embedding=embeddings, index_name=ATLAS_VECTOR_SEARCH)
     return vector_store
+
+def ingest_text(text_content):
+    vector_store = get_vector_store()
+    docs = Document(text_content)
+    vector_store.add_documents([docs])
+    return True
+
+def get_rag_response(query):
+    vector_store = get_vector_store()
+    # might be wrong
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+    retriver = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+    prompt_template = """"Use the following context from the user in order to provide
+    an accurate anwer"""
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_stuff='stuff', retriever=retriver)
+
+    response = qa_chain.invoke({"query": query})
+    return response
